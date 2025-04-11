e221fef28a01547687745f7731da94aab86f3d08
ardazder <3217084099@qq.com>
2025-04-11 13:37:26 +0800

    Site updated: 2025-04-11 13:37:25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1744349767630";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1744349842760";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
